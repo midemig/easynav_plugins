106724e517d70808022425bc73d17f7d0e3deeea
midemig <midemig@users.noreply.github.com>
2025-12-24 11:19:23 +0000

    deploy: 63aa3e50d9fa86afcfb43173176d79d437ca5b40

--- search/all_8.js
+++ search/all_8.js
@@ -4,7 +4,9 @@ var searchData=
   ['hash_3c_20bonxai_3a_3acoordt_20_3e_1',['hash&lt; Bonxai::CoordT &gt;',['../structstd_1_1hash_3_01Bonxai_1_1CoordT_01_4.html',1,'std']]],
   ['headerinfo_2',['HeaderInfo',['../structBonxai_1_1HeaderInfo.html',1,'Bonxai']]],
   ['height_3',['height',['../classeasynav_1_1SimpleMap.html#adc662cea5b05cd9852d5a4538586d662',1,'easynav::SimpleMap']]],
-  ['hit_4',['hit',['../classeasynav_1_1navmap_1_1PerceptionModel.html#a34e0569ddde55bdfaeedc7aded873f97',1,'easynav::navmap::PerceptionModel']]],
-  ['hits_5',['hits',['../structeasynav_1_1Particle.html#a6eb4f6f645392a720b883b687918880f',1,'easynav::Particle::hits'],['../structeasynav_1_1navmap_1_1Particle.html#a1d72d0cbc7314503f28e14041b31ab89',1,'easynav::navmap::Particle::hits'],['../structeasynav_1_1navmap_1_1ScoreAgg.html#a1d72d0cbc7314503f28e14041b31ab89',1,'easynav::navmap::ScoreAgg::hits']]],
-  ['horizon_5fsteps_5f_6',['horizon_steps_',['../classeasynav_1_1MPPIController.html#ad6ba936c86b5a6bebce3778150cb08a8',1,'easynav::MPPIController']]]
+  ['heuristic_5fscale_20tt_4',['Effect of &lt;tt&gt;heuristic_scale&lt;/tt&gt;',['..//github/workspace/planners/easynav_costmap_planner/README.md#autotoc_md203',1,'']]],
+  ['heuristic_5fscale_5f_5',['heuristic_scale_',['../classeasynav_1_1CostmapPlanner.html#a4d32de66d943443ddb2dcb34fb16b718',1,'easynav::CostmapPlanner']]],
+  ['hit_6',['hit',['../classeasynav_1_1navmap_1_1PerceptionModel.html#a34e0569ddde55bdfaeedc7aded873f97',1,'easynav::navmap::PerceptionModel']]],
+  ['hits_7',['hits',['../structeasynav_1_1Particle.html#a6eb4f6f645392a720b883b687918880f',1,'easynav::Particle::hits'],['../structeasynav_1_1navmap_1_1Particle.html#a1d72d0cbc7314503f28e14041b31ab89',1,'easynav::navmap::Particle::hits'],['../structeasynav_1_1navmap_1_1ScoreAgg.html#a1d72d0cbc7314503f28e14041b31ab89',1,'easynav::navmap::ScoreAgg::hits']]],
+  ['horizon_5fsteps_5f_8',['horizon_steps_',['../classeasynav_1_1MPPIController.html#ad6ba936c86b5a6bebce3778150cb08a8',1,'easynav::MPPIController']]]
 ];
--- search/variables_11.js
+++ search/variables_11.js
@@ -1,9 +1,8 @@
 var searchData=
 [
   ['tf_5fbroadcaster_5f_0',['tf_broadcaster_',['../classeasynav_1_1AMCLLocalizer.html#a16637952019b896cbee3ca1ad142d666',1,'easynav::AMCLLocalizer::tf_broadcaster_'],['../classeasynav_1_1navmap_1_1AMCLLocalizer.html#a3bb8aa2a068cff3f94baa49fe4d3365f',1,'easynav::navmap::AMCLLocalizer::tf_broadcaster_']]],
-  ['tf_5fprefix_5f_1',['tf_prefix_',['../classeasynav_1_1CostmapFilter.html#a0f4f0a0a202a235994df2cdab7c40e48',1,'easynav::CostmapFilter::tf_prefix_'],['../classeasynav_1_1navmap_1_1NavMapFilter.html#a0f4f0a0a202a235994df2cdab7c40e48',1,'easynav::navmap::NavMapFilter::tf_prefix_'],['../classeasynav_1_1octomap_1_1OctomapFilter.html#a0f4f0a0a202a235994df2cdab7c40e48',1,'easynav::octomap::OctomapFilter::tf_prefix_']]],
-  ['tolerance_5fdist_5f_2',['tolerance_dist_',['../classeasynav_1_1SimpleController.html#a55a2ffc9006af627f2a4dcddd03af2d0',1,'easynav::SimpleController']]],
-  ['top_5fkeep_5ffraction_5f_3',['top_keep_fraction_',['../classeasynav_1_1navmap_1_1AMCLLocalizer.html#a30467393d6bca4a30afb14edd29b1d41',1,'easynav::navmap::AMCLLocalizer']]],
-  ['twist_5fstamped_5f_4',['twist_stamped_',['../classeasynav_1_1MPPIController.html#a6a9de5bbd5031ab9139b7bb4e69cfd80',1,'easynav::MPPIController::twist_stamped_'],['../classeasynav_1_1SimpleController.html#a6a9de5bbd5031ab9139b7bb4e69cfd80',1,'easynav::SimpleController::twist_stamped_']]],
-  ['type_5fname_5',['type_name',['../structBonxai_1_1HeaderInfo.html#a20f2c4ce97362491655291de4703c42e',1,'Bonxai::HeaderInfo']]]
+  ['tolerance_5fdist_5f_1',['tolerance_dist_',['../classeasynav_1_1SimpleController.html#a55a2ffc9006af627f2a4dcddd03af2d0',1,'easynav::SimpleController']]],
+  ['top_5fkeep_5ffraction_5f_2',['top_keep_fraction_',['../classeasynav_1_1navmap_1_1AMCLLocalizer.html#a30467393d6bca4a30afb14edd29b1d41',1,'easynav::navmap::AMCLLocalizer']]],
+  ['twist_5fstamped_5f_3',['twist_stamped_',['../classeasynav_1_1MPPIController.html#a6a9de5bbd5031ab9139b7bb4e69cfd80',1,'easynav::MPPIController::twist_stamped_'],['../classeasynav_1_1SimpleController.html#a6a9de5bbd5031ab9139b7bb4e69cfd80',1,'easynav::SimpleController::twist_stamped_']]],
+  ['type_5fname_4',['type_name',['../structBonxai_1_1HeaderInfo.html#a20f2c4ce97362491655291de4703c42e',1,'Bonxai::HeaderInfo']]]
 ];
